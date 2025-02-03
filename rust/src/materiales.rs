6bfe457217ec046b6bd7da71cec0efabca71a7aa
DGmikedev <dgmike.dev@gmail.com>
2025-02-03 16:34:48 -0600

    Se adjunta la función días y costos

--- rust/src/materiales.rs
+++ rust/src/materiales.rs
@@ -5,6 +5,7 @@ struct Material<'a> {
     marca: &'a str,
     id_casa_material: u32,
     precio_pieza: (f32, &'a str),
+    piezas_total: f32,
     precio_total: f32,
     tmp_entrga: (u32, &'a str),
     notas: &'a str,
@@ -17,6 +18,7 @@ impl Material<'_> {
         marca: &'a str,
         id_casa_material: u32,
         precio_pieza: (f32, &'a str),
+        piezas_total: f32,
         precio_total: f32,
         tmp_entrga: (u32, &'a str),
         notas: &'a str,
@@ -27,13 +29,16 @@ impl Material<'_> {
             marca,
             id_casa_material,
             precio_pieza,
+            piezas_total,
             precio_total,
             tmp_entrga,
             notas,
         }
     }
 }
+
 fn main() {
+    let mut cost_total_proj: f32 = 0.0;
     let mut material: Vec<Material> = Vec::new();
     let varilla = Material::new(
         1,
@@ -41,6 +46,7 @@ fn main() {
         "s/marca",
         1,
         (13.20, "mt"),
+        34.0,
         5002.0,
         (1, "semana"),
         "",
@@ -51,6 +57,7 @@ fn main() {
         "CRUZ AZUL",
         1,
         (250.0, "BULTO 50"),
+        100.0,
         40000.00,
         (1, "semana"),
         "no mojar",
@@ -59,5 +66,32 @@ fn main() {
     material.push(varilla);
     material.push(cemento);
 
-    println!("{:#?}", material);
+    // generador costos
+    for costo in material.into_iter() {
+        cost_total_proj += costo.precio_total
+    }
+
+    println!("{cost_total_proj}");
+    let dias: f32 = get_dias(&2.5, "semana");
+    println!("{dias} dias");
+    // let costo:f32 = get_costo(&material[0]);
+    //println!("{:#?}", material);
+}
+
+fn get_dias(cantidad: &f32, unidad: &str) -> f32 {
+    let mut dias: f32 = 0f32;
+    match unidad {
+        "horas" => dias = (cantidad / 24.0),
+        "dia" => dias = 1.0,
+        "semana" => dias = 7.0,
+        "mes" => dias = 30.0,
+        &_ => dias = 365.0,
+    }
+    dias = dias * cantidad;
+    dias
+}
+
+fn get_costo(costo: &f32) -> f32 {
+    println!("{:#?}", costo);
+    10.0
 }
