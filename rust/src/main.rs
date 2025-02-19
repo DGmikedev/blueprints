c7d54cb7057bc450c685d732d33edb7387f673a4
DGmikedev <dgmike.dev@gmail.com>
2025-02-18 19:40:44 -0600

    refactory part 2.1

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,13 +3,14 @@ mod tokens;
 
 fn main(){
 
+
+    // EJEMPLOS -- BORRAR AL FINAL DEL TEST
     let token:String = tokens::genera_token(24);
     println!("{token}");
 
     let vector: Vec<f32> = vec![1.0, 2.0, 3.0];
     let g = media(&vector);
-
-    
+   
     println!("{g}");
 
 }
