ffeb24d47f6b743b472f44cf309b0c2826928ed3
DGmikedev <dgmike.dev@gmail.com>
2025-02-18 19:40:17 -0600

    refactory part 2

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,11 +2,23 @@ mod tokens;
 
 
 fn main(){
-    
 
     let token:String = tokens::genera_token(24);
     println!("{token}");
 
+    let vector: Vec<f32> = vec![1.0, 2.0, 3.0];
+    let g = media(&vector);
 
+    
+    println!("{g}");
 
 }
+
+
+// SECCIÓN ESTADISTICA
+
+fn media(vector:&[f32])->f32{ 
+    let media = vector.iter().sum::<f32>() / vector.len() as f32; 
+    media 
+}
+
