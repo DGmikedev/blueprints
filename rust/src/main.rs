2a541c5d4bda1780b6d912eeaa374970f559ee55
DGmikedev <dgmike.dev@gmail.com>
2025-02-20 19:37:03 -0600

    adding maximos, minimos y rango functions

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -17,9 +17,15 @@ fn main(){
     let media = media(&vector);
     let mediana  = mediana(&vector);
     let modas:Vec<(usize, f32)> = moda(&vector);
+    let desviacion_std:f32 = desv_std(&vector, &media);
+    let varianzas: [f32;2] = varianza(&vector, &media);
+    let rango:f32 = rango(&vector);
+    let mayor:f32 = valor_maximo(&vector);
+    let minimo:f32 = valor_minimo(&vector);
+
 
    pr_sep!();
-   let msg: String = format!("Datos: {:?}\nMedia: {}\nMediana: {}\nModas: {:?}", vector, media, mediana, modas);
+   let msg: String = format!("Datos: {:?}\nMedia: {}\nMediana: {}\nModas: {:?}\nDeviacion_std: {}\nVarianzas: {:?}\nRango: {}\nValor mayor: {}\nValor minimo: {} \n", vector, media, mediana, modas, desviacion_std, varianzas,rango, mayor, minimo);
    pr_sep!(msg);
 }
 
@@ -121,6 +127,96 @@ fn moda(vector_i:&Vec<f32>)-> Vec<(usize, f32)>{
 
 }
 
+#[doc=r"Devuelve el calculo de la desviación estandar en un f32 de un vector<f32> referenciado"]
+pub fn desv_std(val:&Vec<f32> , med: &f32) -> f32 {
+
+    let mut var:f32 = 0.0;
+    val.iter()
+        .for_each(|x: &f32|{ 
+            var = var + (x - med).powi(2);
+        }); 
+        var = var/(val.len() as f32 - 1f32);
+        var = var.sqrt();
+        var
+}
+
+#[doc=r"Devuleve un vector<f32,f32> con un vector referenciado<f32> y la media<f32> ambos referenciados"]
+pub fn varianza(val:&Vec<f32> , med: &f32) -> [f32; 2] {
+
+    let mut var:f32 = 0.0;
+    val.into_iter()
+        .for_each(|x: &f32|{ 
+            var += (x - med).powi(2);
+         }); 
+    [var /(val.len() as f32), var /(val.len() as f32 - 1f32)]
+}
+
+#[doc=r"Devuelve el valor maximo<f32> contenido en un vector<f32> referenciado"]
+fn valor_maximo(vec:&Vec<f32>)-> f32{
+    let mut maximo: f32 = 0.0;
+    if let Some(&valor_maximo) = vec.iter()
+            .max_by(|a,b| a.partial_cmp(b).unwrap()){
+        maximo = valor_maximo.clone();
+        return maximo
+    }else{ 
+        return maximo
+    }
+}
+
+#[doc=r"Devuelve el valor minimo<f32> contenido en un vector<f32> referenciado"]
+fn valor_minimo(vec:&Vec<f32>)-> f32{
+    let mut minimo: f32 = 0.0;
+    if let Some(&valor_minimo) = vec.iter()
+            .min_by(|a,b| a.partial_cmp(b).unwrap()){
+        minimo = valor_minimo.clone();
+        return minimo
+    }else{ 
+        return minimo
+    }
+}
+#[doc=r"Devuelve el rango el mayor<f32> - el menor<f32> de un vector<f32> referenciado"]
+fn rango(vector:&Vec<f32>)-> f32{
+    let maximo = valor_maximo(vector);
+    let minimo = valor_minimo(vector);
+    return maximo - minimo
+}
+
+/*
+fn main() {
+    let vec: Vec<f32> = vec![10.5, 23.3, 5.7, 17.2, 42.9];
+
+    // Obtener el valor máximo
+    if let Some(&max_value) = vec.iter().max_by(|a, b| a.partial_cmp(b).unwrap()) {
+        println!("Valor máximo: {}", max_value);
+    } else {
+        println!("El vector está vacío");
+    }
+
+    // Obtener el valor mínimo
+    if let Some(&min_value) = vec.iter().min_by(|a, b| a.partial_cmp(b).unwrap()) {
+        println!("Valor mínimo: {}", min_value);
+    } else {
+        println!("El vector está vacío");
+    }
+}
+
+
+*/
+
+
+#[doc=r"Devuelve un vector<V<V<f32>> de vectores referenciado, asemejando una matriz, esta matriz devuleta será transpuesta"]
+fn transpuestafn( arr:&Vec<Vec<f32>> )->Vec<Vec<f32>>{
+
+    let high:usize = arr.len();
+    let with:usize = arr[0].len();
+    let mut arr_return: Vec<Vec<f32>> = vec![vec![0.0; high]; with];
+        for (i, vect) in arr.into_iter().enumerate(){ 
+            for j in 0..vect.len(){
+                arr_return[j][i] = arr[i][j]; 
+            }
+        }
+    arr_return
+}
 
 fn igualf32(a: f32, b: f32) -> bool {
     let epsilon = f32::EPSILON * 1000.0; // Ajusta este valor si es necesario
@@ -135,6 +231,9 @@ fn igualf32(a: f32, b: f32) -> bool {
 // | respons |.unwrap() -> extrae el resultado, es un tipo Option
 // | vec     |.contains -> contiene
 // | vec     |let mut hashmap: HashMap<K, V>= HashMap::new(); -> declara u hashmap -> fn nombre_funcion<K, V> determinar el contexto
+// | vec     |if let Some(&valor_maximo) = vec.iter().max_by(|a,b| a.partial_cmp(b).unwrap()){   -> Some es un contenedor de respuesta si es exitoso lo guarda en &valor_maximo del if
+// |         |y si no lo guarda en el else,  ->max_by(|a,b| a.prtial_cmp(b)...)  obtiene el maximo comparando parcialmete a y b
+
 
 
 
